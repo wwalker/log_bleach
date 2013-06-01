480b7147a91c38d98d1f6805d9cf016d54f7ada6
Wayne Walker <wwalker@solid-constructs.com>
2013-05-31 20:51:41 -0500

    still seems to work

--- signature_to_egrep_regex.rb
+++ signature_to_egrep_regex.rb
@@ -8,17 +8,17 @@ TS_SYSLOG_WITH_WEEKDAY     = '(?:Sun|Mon|Tue|Wed|Thu|Fri|Sat)\s+' + TS_SYSLOG
 SYSLOG_PREFIX              = TS_SYSLOG + ' \S+ \S+(?:\[\d+\])?: '
 ISODATE                    = '\d\d\d\d-\d\d-\d\d[ T]\d\d:\d\d:\d\d'
 ISODATE_WITH_USEC          = '\d\d\d\d-\d\d-\d\d[ T]\d\d:\d\d:\d\d.\d\d\d\d\d\d'
-RN_LOG_PREFIX              = ISODATE_WITH_USEC + '\s+\d+\s+(DEBUG|WARNING|NOTICE|INFO|CRITICAL|ERROR) -- \d+\s+\| '
+RN_LOG_PREFIX              = ISODATE_WITH_USEC + '\s+\d+\s+(LINE|DEBUG|WARNING|NOTICE|INFO|CRITICAL|ERROR)\s+--\s+\d+\s+\| '
 HEXNUM                     = '[A-Fa-f0-9][A-Fa-f0-9]+'
 FLOAT                      = '[\-+]?\d+\.\d+(?:[eE][\-+]?\d+)?'
 INTEGER                    = '[\-+]?\d+'
-REGEX_SPECIAL_CHAR         = '[\+\{\}\(\)\[\]\$\^\?\\\\\*\`]'
+REGEX_SPECIAL_CHAR         = '[\|\+\{\}\(\)\[\]\$\^\?\\\\\*\`]'
 FULL_FILE_PATH             = '(?:/[^/]+)+/?'
 RUBY_STACK_TRACE_FILE_LINE = FULL_FILE_PATH + ':\d+:in `\S+' + "\'`"
 WHITESPACE_REGION          = '\s+\s'
 
 STDIN.lines.each do |line|
-  line.chomp!
+#  line.chomp!
   # Find datestamp if any and replace it
   line.gsub!(/#{RN_LOG_PREFIX}/              , '<%= RN_LOG_PREFIX %>')
   line.gsub!(/#{ISODATE_WITH_USEC}/          , '<%= ISODATE_WITH_USEC %>')
@@ -33,7 +33,7 @@ STDIN.lines.each do |line|
   line.gsub!(/#{RUBY_STACK_TRACE_FILE_LINE}/ , '<%= RUBY_STACK_TRACE_FILE_LINE %>')
 #  line.gsub!(/#{WHITESPACE_REGION}/          , '<%= WHITESPACE_REGION %>')
   line.gsub!(/(#{REGEX_SPECIAL_CHAR})/) { |m| '\\' + m }
-  line = '\\A' + line + '\\s*\\Z'
+#  line = '\\A' + line + '\\s*\\Z'
   puts line
   STDERR.puts ERB.new(line).result
 end
